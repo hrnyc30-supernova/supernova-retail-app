dd6b0189424f4c8373e2b45e40c234f9af5ce3d3
Chris Campos <camposchris18@gmail.com>
2020-07-02 15:25:10 -0400

    Questions And Answers General Structure Should Render

---
+++ src/components/questions-and-answers/questionDisplay.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class QuestionDisplay extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <h1>Questions Display Component</h1>;
+  }
+}
+
+export default QuestionDisplay;
