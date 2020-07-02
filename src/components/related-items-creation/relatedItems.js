476fc9b611a770a72b4f16ff99e1e9066a9e1856
Henry Wang <henry.h.wang93@gmail.com>
2020-07-01 22:31:26 -0400

    update state with related product ids upon mount

--- src/components/related-items-creation/relatedItems.js
+++ src/components/related-items-creation/relatedItems.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import apiMaster from '../../apiMaster';
 
 class RelatedItems extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      relatedProductIds: [],
+    };
+
+    this.getRelatedIds = this.getRelatedIds.bind(this);
   }
+
+  componentDidMount() {
+    // console.log('apiMaster: ', apiMaster);
+    this.getRelatedIds();
+  }
+
+  getRelatedIds() {
+    apiMaster
+      .getRelatedProducts(this.props.currentProductID)
+      .then((ids) => {
+        this.setState({ relatedProductIds: ids.data });
+      })
+      .catch((err) => {
+        console.log('err in getRelatedIds: ', err);
+      });
+  }
+
   render() {
-    return <h1>Related Products</h1>;
+    return (
+      <>
+        <h1>Related Products</h1>
+        <div>{this.props.currentProductID}</div>
+      </>
+    );
   }
 }
 
