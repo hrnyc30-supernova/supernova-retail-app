a3c05bd81368fa6c52a4bb668f998f34e5f65296
Henry Wang <henry.h.wang93@gmail.com>
2020-07-02 15:48:45 -0400

    set up basic card  structure

--- src/components/related-items-creation/productCard.js
+++ src/components/related-items-creation/productCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import apiMaster from '../../apiMaster';
+import Card from 'react-bootstrap/Card';
 
 class ProductCard extends React.Component {
   constructor(props) {
@@ -9,13 +10,21 @@ class ProductCard extends React.Component {
     };
 
     this.getCardDetails = this.getCardDetails.bind(this);
+    this.clearList = this.clearList.bind(this);
   }
 
   componentDidUpdate(prevProps) {
     if (this.props.relatedProducts !== prevProps.relatedProducts) {
-      console.log('props: ', this.props.relatedProducts);
+      // console.log('props: ', this.props.relatedProducts);
+      this.clearList();
       this.getCardDetails();
     }
+  }
+
+  clearList() {
+    this.setState({
+      cardDetails: [],
+    });
   }
 
   getCardDetails() {
@@ -36,10 +45,17 @@ class ProductCard extends React.Component {
   }
 
   render() {
-    // console.log('props: ', this.props);
     return (
       <>
-        <p>This will be a product card</p>
+        <Card style={{ width: '15rem' }}>
+          <Card.Img variant="top" src="" />
+          <Card.Text>Super awesome image placeholder</Card.Text>
+          <Card.Body>
+            <Card.Subtitle>Category</Card.Subtitle>
+            <Card.Title>Expanded Product Name With Extra Text</Card.Title>
+            <Card.Text>Price</Card.Text>
+          </Card.Body>
+        </Card>
       </>
     );
   }
