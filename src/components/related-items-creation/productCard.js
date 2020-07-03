b0d81433338c5b2f29f1418fa5cf572258cd3528
Henry Wang <henry.h.wang93@gmail.com>
2020-07-03 00:41:02 -0400

    render main image in product cards

--- src/components/related-items-creation/productCard.js
+++ src/components/related-items-creation/productCard.js
@@ -7,10 +7,13 @@ class ProductCard extends React.Component {
     super(props);
     this.state = {
       cardDetails: [],
+      cardImages: [],
+      test: ['kitty', 'puppy', 'turtle', 'dolphin'],
     };
 
-    this.getCardDetails = this.getCardDetails.bind(this);
     this.clearList = this.clearList.bind(this);
+    this.getCardDetails = this.getCardDetails.bind(this);
+    this.getCardImages = this.getCardImages.bind(this);
   }
 
   componentDidUpdate(prevProps) {
@@ -18,12 +21,14 @@ class ProductCard extends React.Component {
       // console.log('props: ', this.props.relatedProducts);
       this.clearList();
       this.getCardDetails();
+      this.getCardImages();
     }
   }
 
   clearList() {
     this.setState({
       cardDetails: [],
+      cardImages: [],
     });
   }
 
@@ -44,14 +49,32 @@ class ProductCard extends React.Component {
     }
   }
 
+  getCardImages() {
+    for (let i = 0; i < this.props.relatedProducts.length; i++) {
+      apiMaster
+        .getProductStyles(this.props.relatedProducts[i])
+        .then((info) => {
+          this.setState((prevState) => ({
+            cardImages: [
+              ...prevState.cardImages,
+              info.data.results[0].photos[0].thumbnail_url ||
+                'https://images.unsplash.com/photo-1529088148495-2d9f231db829?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1100&q=80',
+            ],
+          }));
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }
+
   render() {
     return (
       <div className="cards-wrapper">
         {this.state.cardDetails.map((card, i) => {
           return (
             <Card style={{ width: '15rem' }} key={i}>
-              <Card.Img variant="top" src="" />
-              <Card.Text>Super awesome image placeholder</Card.Text>
+              <Card.Img variant="top" src={this.state.cardImages[i]} />
               <Card.Body>
                 <Card.Subtitle>{card.category}</Card.Subtitle>
                 <Card.Title>{card.name}</Card.Title>
