1790dcfe549d391ffbbd80ecb72c458efabc915b
Shannon Connolly <shannonconnolly92@gmail.com>
2020-07-03 12:51:15 -0400

    Render rating characteristics to page

--- src/components/ratings-and-reviews/ratingsBreakdown.js
+++ src/components/ratings-and-reviews/ratingsBreakdown.js
@@ -9,16 +9,19 @@ class RatingsBreakdown extends React.Component {
     constructor(props) {
         super(props);
         this.state ={
-            currentRating: {}
-        }
+            currentRating: {},
+            characteristics: []
+        };
+        this.getCharacteristics = this.getCharacteristics.bind(this);
     }
 
     componentDidMount() {
         apiMaster.getReviewMetaData(this.props.currentProductId)
             .then(({ data }) => {
-                console.log(data);
+                let chars = this.getCharacteristics();
                 this.setState({
-                    currentRating: data
+                    currentRating: data,
+                    characteristics: chars
                 })
             })
             .catch(err => {
@@ -27,19 +30,25 @@ class RatingsBreakdown extends React.Component {
     }
 
     getCharacteristics() {
+        let chars
         if (this.state.currentRating.characteristics !== undefined) {
             let chars = Object.keys(this.state.currentRating.characteristics);
-            console.log('should be an array', chars);
             let charString = chars.join(', ');
             return charString;
-        } else {
-            return null;
-        }
+        } 
+        return null;
     }
+
     render() {
+        console.log(this.state.currentRating.characteristics);
+        let chars = this.state.currentRating.characteristics;
         return (
             <div className='ratings-breakdown-container'>Current Product Rating Breakdown: 
-                <p>{this.getCharacteristics()}</p>
+                <p>{chars !== undefined ? (chars.Size !== undefined ? `Size: ${chars.Size.value}` : 'Does not have a size') : null}</p>
+                <p>{chars !== undefined ? (chars.Fit !== undefined ? `Fit: ${chars.Fit.value}` : 'Does not have a Fit') : null}</p>
+                <p>{chars !== undefined ? (chars.Length !== undefined ? `Length: ${chars.Length.value}` : 'Does not have a Length') : null}</p>
+                <p>{chars !== undefined ? (chars.Quality !== undefined ? `Quality: ${chars.Quality.value}` : 'Does not have a Quality') : null}</p>
+                <p>{chars !== undefined ? (chars.Comfort !== undefined ? `Comfort: ${chars.Comfort.value}` : 'Does not have a Comfort') : null}</p>
                 <Stars rating={this.props.averageRating} />
             </div>
         );
