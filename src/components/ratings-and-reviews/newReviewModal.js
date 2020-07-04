979a51c8765b07a6bedfda838da0367654dcff0d
Shannon Connolly <shannonconnolly92@gmail.com>
2020-07-04 08:07:15 -0400

    Add all required categories to new review modal

--- src/components/ratings-and-reviews/newReviewModal.js
+++ src/components/ratings-and-reviews/newReviewModal.js
@@ -6,9 +6,20 @@ class NewReview extends React.Component {
   constructor(props) {
     super(props);
     this.state ={
-      
+      rating: 0, 
+      recommendation: '',
+      characteristics: {},
+      summary: '',
+      body: '',
+      photos: {},
+      nickname: '',
+      email: ''
     };
-    
+    this.sendReview = this.sendReview.bind(this);
+  }
+
+  sendReview(reviewObj) {
+    console.log('this is the review that we will send to the API', reviewObj);
   }
 
   render() {
@@ -17,6 +28,14 @@ class NewReview extends React.Component {
       <div>
           <h4>Write Your Review</h4><h6>{`About the ${this.props.currentProductName}`}</h6>
           <p>Overall Rating <Stars/></p>
+          <p>Recommendation</p>
+          <p>Characteristics</p>
+          <p>Review Summary</p>
+          <p>Review Body</p>
+          <p>Upload Photos</p>
+          <p>Nickname</p>
+          <p>Email</p>
+          <button onClick={e => this.sendReview(this.state)}>Submit Review</button>
 
       </div>
     );
