a51654357591f0b2c32d9d6054c2eea9fbfbff63
Shannon Connolly <shannonconnolly92@gmail.com>
2020-07-04 15:38:19 -0400

    Add text area for review summary and body in new review modal

--- src/components/ratings-and-reviews/newReviewModal.js
+++ src/components/ratings-and-reviews/newReviewModal.js
@@ -29,19 +29,21 @@ class NewReview extends React.Component {
         <div>
             <h4>Write Your Review</h4><h6>{`About the ${this.props.currentProductName}`}</h6>
             <form>
-                <p>*Overall Rating <Stars/></p>
+                <p>*Overall Rating <Stars rating='0' /></p>
                 <label htmlFor='recommend'>*Do you recommend this product?</label>
-                <input type="radio" name="recommend" value="yes"/> Yes
-                <input type="radio" name="recommend" value="no"/> No<br/>
+                <input type="radio" name="recommend" id="recommend" value="yes"/> Yes
+                <input type="radio" name="recommend" id="recommend"value="no"/> No<br/>
                 <label htmlFor='characteristics'>*Characteristics</label><br/>
                 {Object.keys(this.props.currentProductCharacteristics).map(char => {
                     return <><label htmlFor={char}>{`${char}: `}</label>{[...Array(5)].map((item, i) => {
                         return <><label htmlFor={char}>{i + 1}</label><input type="radio" name={char} value={i + 1}/></> 
 
                     })}</>
-                })}
-                <p>*Review Summary</p>
-                <p>*Review Body</p>
+                })} <br/>
+                <label htmlFor='summary'>*Review Summary</label><br/>
+                <textarea type='text' id='summary' name='summary' placeholder='Example: Best purchase ever!' maxLength='60'></textarea><br/>
+                <label htmlFor='body'>*Review Body</label><br/>
+                <textarea type='text' id='body' name='summary' placeholder='Why did you like the product or not?' maxLength='1000' minLength='50'></textarea>
                 <p>Upload Photos</p>
                 <p>*Nickname</p>
                 <p>Email</p>
