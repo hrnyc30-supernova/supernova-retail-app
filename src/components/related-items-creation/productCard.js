58bc7a10c519077f5c1d7f5109e9b12fed146457
Henry Wang <henry.h.wang93@gmail.com>
2020-07-04 14:31:58 -0400

    created card carousel structure for related products

--- src/components/related-items-creation/productCard.js
+++ src/components/related-items-creation/productCard.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import apiMaster from '../../apiMaster';
+import Stars from './stars';
+import ItemsCarousel from 'react-items-carousel';
+import {
+  FaRegArrowAltCircleLeft,
+  FaRegArrowAltCircleRight,
+} from 'react-icons/fa';
 
 class ProductCard extends React.Component {
   constructor(props) {
@@ -11,6 +17,7 @@ class ProductCard extends React.Component {
       cardDetailsLoaded: false,
       cardImagesLoaded: false,
       cardPricesLoaded: false,
+      activeItemIndex: 0,
     };
 
     this.getCardDetails = this.getCardDetails.bind(this);
@@ -100,63 +107,86 @@ class ProductCard extends React.Component {
     const arePricesLoaded = this.state.cardPricesLoaded;
     if (areDetailsLoaded && areImagesLoaded && arePricesLoaded) {
       return (
-        <div className="cards-wrapper">
-          {this.state.cardDetails.map((card, i) => {
-            // console.log('this.state.cardDetails: ', this.state.cardDetails);
-            // console.log('this.state.cardPrices: ', this.state.cardPrices);
-            return (
-              <div className="card" style={{ width: '15rem' }} key={i}>
-                <img className="card-img-top" src={this.state.cardImages[i]} />
-                <div className="card-body">
-                  <div className="card-subtitle">{card.category}</div>
-                  <div className="card-title">{card.name}</div>
-                  <span
-                    className={
-                      this.state.cardPrices[i].sale_price === '0'
-                        ? 'discounted-price-hidden'
-                        : ''
-                    }
-                    style={
-                      ({
-                        textDecoration:
-                          this.state.cardPrices[i].sale_price !== '0'
-                            ? 'line-through'
-                            : 'none',
-                      },
-                      { color: 'red' })
-                    }
-                  >
-                    {Number(this.state.cardPrices[i].sale_price).toLocaleString(
-                      'en-US',
-                      {
+        <div className="carousel-wrapper">
+          <ItemsCarousel
+            infiniteLoop={false}
+            gutter={20}
+            activePosition={'center'}
+            chevronWidth={60}
+            disableSwipe={false}
+            alwaysShowChevns={false}
+            numberOfCards={3}
+            slidesToScroll={1}
+            outsideChevron={true}
+            showSlither={true}
+            firstAndLastGutter={true}
+            activeItemIndex={this.state.activeItemIndex}
+            requestToChangeActive={(value) =>
+              this.setState({ activeItemIndex: value })
+            }
+            rightChevron={<FaRegArrowAltCircleRight color="#525252" />}
+            leftChevron={<FaRegArrowAltCircleLeft color="#525252" />}
+          >
+            {this.state.cardDetails.map((card, i) => {
+              return (
+                <div className="card" key={i}>
+                  <img
+                    className="card-img-top"
+                    src={this.state.cardImages[i]}
+                  />
+                  <div className="card-body">
+                    <div className="card-subtitle">{card.category}</div>
+                    <div className="card-title">{card.name}</div>
+                    <span
+                      className={
+                        this.state.cardPrices[i].sale_price === '0'
+                          ? 'discounted-price-hidden'
+                          : ''
+                      }
+                      style={
+                        ({
+                          textDecoration:
+                            this.state.cardPrices[i].sale_price !== '0'
+                              ? 'line-through'
+                              : 'none',
+                        },
+                        { color: 'red' })
+                      }
+                    >
+                      {Number(
+                        this.state.cardPrices[i].sale_price
+                      ).toLocaleString('en-US', {
                         style: 'currency',
                         currency: 'USD',
                         minimumFractionDigits: 0,
                         maximumFractionDigits: 0,
-                      }
-                    )}
-                  </span>
-                  <span
-                    style={{
-                      textDecoration:
-                        this.state.cardPrices[i].sale_price !== '0'
-                          ? 'line-through'
-                          : 'none',
-                    }}
-                  >
-                    {Number(
-                      this.state.cardPrices[i].original_price
-                    ).toLocaleString('en-US', {
-                      style: 'currency',
-                      currency: 'USD',
-                      minimumFractionDigits: 0,
-                      maximumFractionDigits: 0,
-                    })}
-                  </span>
+                      })}
+                    </span>
+                    <span
+                      style={{
+                        textDecoration:
+                          this.state.cardPrices[i].sale_price !== '0'
+                            ? 'line-through'
+                            : 'none',
+                      }}
+                    >
+                      {Number(
+                        this.state.cardPrices[i].original_price
+                      ).toLocaleString('en-US', {
+                        style: 'currency',
+                        currency: 'USD',
+                        minimumFractionDigits: 0,
+                        maximumFractionDigits: 0,
+                      })}
+                    </span>
+                  </div>
+                  <div className="stars-body">
+                    <Stars />
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </ItemsCarousel>
         </div>
       );
     } else {
