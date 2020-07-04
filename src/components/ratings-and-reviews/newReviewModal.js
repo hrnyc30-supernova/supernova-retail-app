11d09e862744e59d9ff2862f08da9fbd16d76d01
Shannon Connolly <shannonconnolly92@gmail.com>
2020-07-04 17:12:50 -0400

    Add scale for each characteristic on new review modal

--- src/components/ratings-and-reviews/newReviewModal.js
+++ src/components/ratings-and-reviews/newReviewModal.js
@@ -7,7 +7,7 @@ class NewReview extends React.Component {
     super(props);
     this.state ={
       rating: 0, 
-      recommendation: '',
+      recommend: 1,
       characteristics: {},
       summary: '',
       body: '',
@@ -15,43 +15,98 @@ class NewReview extends React.Component {
       nickname: '',
       email: ''
     };
+    this.charScales = {
+      Size: {
+        '1': 'A size too small',
+        '2': '1/2 a size too small',
+        '3': 'Perfect',
+        '4': '1/2 a size too big',
+        '5': 'A size too big'
+      }, 
+      Width: {
+        '1': 'Too narrow',
+        '2': 'Slightly narrow',
+        '3': 'Perfect',
+        '4': 'Slightly Wide',
+        '5': 'Too wide'
+      },
+      Comfort: {
+        '1': 'Uncomfortable',
+        '2': 'Slightly uncomfortable',
+        '3': 'Ok',
+        '4': 'Comfortable',
+        '5': 'Perfect'
+      },
+      Quality: {
+        '1': 'Poor',
+        '2': 'Below average',
+        '3': 'What I expected',
+        '4': 'Pretty great',
+        '5': 'Perfect'
+      },
+      Length: {
+        '1': 'Runs short',
+        '2': 'Runs slightly short',
+        '3': 'Perfect',
+        '4': 'Runs slightly long',
+        '5': 'Runs long'
+      },
+      Fit: {
+        '1': 'Runs tight',
+        '2': 'Runs slightly tight',
+        '3': 'Perfect',
+        '4': 'Runs slightly big',
+        '5': 'Runs big'
+      }
+    }
     this.sendReview = this.sendReview.bind(this);
+    this.getScaleValue = this.getScaleValue.bind(this);
   }
 
   sendReview(reviewObj) {
     // console.log('this is the review that we will send to the API', reviewObj);
   }
 
+  getScaleValue(characteristic, rating) {
+    rating = rating.toString();
+    return this.charScales[characteristic][rating];
+  }
+
   render() {
-      console.log(this.props.currentProductCharacteristics);
     if (this.props.show === false) return null;
     return (
         <div>
             <h4>Write Your Review</h4><h6>{`About the ${this.props.currentProductName}`}</h6>
             <form>
                 <p>*Overall Rating <Stars rating='0' /></p>
-                <label htmlFor='recommend'>*Do you recommend this product?</label>
-                <input type="radio" name="recommend" id="recommend" value="yes"/> Yes
-                <input type="radio" name="recommend" id="recommend"value="no"/> No<br/>
-                <label htmlFor='characteristics'>*Characteristics</label><br/>
+                <label className='label-container' htmlFor='recommend' required>*Do you recommend this product?
+                    <input type="radio" name="recommend" id="recommend" default value="1" /> Yes
+                    <input type="radio" name="recommend" id="recommend"value="0" /> No<br/>
+                </label>
+                <label className='label-container' htmlFor='characteristics'>*Characteristics<br/>
                 {Object.keys(this.props.currentProductCharacteristics).map(char => {
-                    return <><label htmlFor={char}>{`${char}: `}</label>{[...Array(5)].map((item, i) => {
-                        return <><label htmlFor={char}>{i + 1}</label><input type="radio" name={char} value={i + 1}/></> 
+                    console.log('char', char)
+                    return <><label className='label-container' required htmlFor={char}>{`${char}: `}</label>{['1', '2', '3', '4', '5'].map((item, i) => {
+                        console.log(char);
+                        return <><input type="radio" name={char} value={i+1}/><small>{this.getScaleValue(char, item)}</small></> 
 
-                    })}</>
-                })} <br/>
-                <label htmlFor='summary'>*Review Summary</label><br/>
-                <textarea type='text' id='summary' name='summary' placeholder='Example: Best purchase ever!' maxLength='60'></textarea><br/>
-                <label htmlFor='body'>*Review Body</label><br/>
-                <textarea type='text' id='body' name='body' placeholder='Why did you like the product or not?' maxLength='1000' minLength='50'></textarea>
-                <p>Upload Photos</p>
-                <label htmlFor='nickname'>*Nickname</label><br/>
-                <textarea type='text' id='nickname' name='nickname' placeholder='Example: jackson11!' maxLength='60'></textarea><br/>
-                <small>For privacy reasons, do not use your full name or email address</small><br/>
-                <label htmlFor='email'>*Email</label><br/>
-                <textarea type='text' id='email' name='email' placeholder='Example: jackson11@email.com' maxLength='60'></textarea><br/>
-                <small>For authentication reasons, you will not be emailed</small>
-                <p>Email</p>
+                    })}<br/></>
+                })} </label><br/>
+                <label className='label-container' required htmlFor='summary'>*Review Summary<br/>
+                    <textarea type='text' id='summary' required name='summary' placeholder='Example: Best purchase ever!' maxLength='60'></textarea>
+                </label><br/>
+                <label className='label-container' required htmlFor='body'>*Review Body<br/>
+                    <textarea type='text' id='body' required name='body' placeholder='Why did you like the product or not?' maxLength='1000' minLength='50'></textarea>
+                </label>
+                <p>Upload your photos</p>
+                <label className='label-container' required htmlFor='nickname'>*What is your nickname<br/>
+                    <textarea type='text' id='nickname' required name='nickname' placeholder='Example: jackson11!' maxLength='60'></textarea><br/>
+                    <small>For privacy reasons, do not use your full name or email address</small><br/>
+                </label><br/>
+                <label className='label-container' required htmlFor='email'>*Your email<br/>
+                    <textarea type='text' id='email' required name='email' placeholder='Example: jackson11@email.com' maxLength='60'></textarea><br/>
+                    <small>For authentication reasons, you will not be emailed</small>
+                </label>
             </form>
           <button onClick={e => this.sendReview(this.state)}>Submit Review</button>
         </div>
