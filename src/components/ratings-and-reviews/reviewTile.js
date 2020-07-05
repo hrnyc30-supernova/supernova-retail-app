3147fbdebcc8e20133f144830719c7b5416c840c
Shannon Connolly <shannonconnolly92@gmail.com>
2020-07-05 16:32:18 -0400

    Create thumbnail images on each review

--- src/components/ratings-and-reviews/reviewTile.js
+++ src/components/ratings-and-reviews/reviewTile.js
@@ -20,7 +20,6 @@ class ReviewTile extends React.Component {
     }
 
     render() {
-        console.log(this.props.review);
         let date = this.props.review ? moment(this.props.review.date).format("MMMM D, YYYY"): null;
         return(
             <div id='review-tile-container'>
@@ -30,7 +29,7 @@ class ReviewTile extends React.Component {
                     <p><strong>{this.props.review.summary.length <= 60 ? this.props.review.summary : this.props.review.summary.slice(0, 61)}</strong></p>
                     <p>{this.props.review.body.length > 250 ? (this.state.showAll === false ? <>{this.props.review.body.slice(0, 251)}<br/><span className='link' onClick={this.handleClick}>Show More</span></> : <>{this.props.review.body}<br/><span className='link' onClick={this.handleClick}>Show Less</span></>) : this.props.review.body}</p>
                     <>{this.props.review.photos.map((photo) => {
-                        return <img key={photo.id} src={photo.url} />
+                        return <img className='review-photo' key={photo.id} src={photo.url} />
                     })}</>
                     <p>{this.props.review.recommend === 0 ? <><FaCheck size='.75em'/>I recommend this product</> : null}</p>
                     {this.props.review.response === null ? null : <p className='review-response'>Response from seller: {this.props.review.response}</p>}
