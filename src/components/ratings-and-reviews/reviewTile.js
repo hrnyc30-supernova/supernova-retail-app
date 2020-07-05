a5f87641f5460110599a282d2902c2dca771f31a
Shannon Connolly <shannonconnolly92@gmail.com>
2020-07-05 17:08:56 -0400

    Add  modal to hold bigger image when thumbnail image within body of review is clicked

--- src/components/ratings-and-reviews/reviewTile.js
+++ src/components/ratings-and-reviews/reviewTile.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Stars from './stars.js';
+import Modal from 'react-bootstrap/Modal'
 import moment from 'moment';
 import { FaCheck, FaCheckCircle } from 'react-icons/fa';
 
@@ -7,15 +8,24 @@ class ReviewTile extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            showAll: false
+            showAll: false,
+            showImgModal: false
         }; 
         this.handleClick = this.handleClick.bind(this);
+        this.handleImgClick = this.handleImgClick.bind(this);
     }
 
     handleClick() {
         let temp = this.state.showAll;
         this.setState({
             showAll: !temp
+        })
+    }
+
+    handleImgClick(e) {
+        let temp = this.state.showImgModal;
+        this.setState({
+            showImgModal: !temp
         })
     }
 
@@ -29,8 +39,14 @@ class ReviewTile extends React.Component {
                     <p><strong>{this.props.review.summary.length <= 60 ? this.props.review.summary : this.props.review.summary.slice(0, 61)}</strong></p>
                     <p>{this.props.review.body.length > 250 ? (this.state.showAll === false ? <>{this.props.review.body.slice(0, 251)}<br/><span className='link' onClick={this.handleClick}>Show More</span></> : <>{this.props.review.body}<br/><span className='link' onClick={this.handleClick}>Show Less</span></>) : this.props.review.body}</p>
                     <>{this.props.review.photos.map((photo) => {
-                        return <img className='review-photo' key={photo.id} src={photo.url} />
+                        return <img className='review-photo' key={photo.id} src={photo.url} value={photo.url} onClick={e => this.handleImgClick(e)}/>
                     })}</>
+                    <Modal show={this.state.showImgModal} animation={false} onHide={e => this.handleImgClick(e)}>
+                        <Modal.Header closeButton/>
+                        <Modal.Body>
+                            Image goes here!
+                        </Modal.Body>
+                    </Modal>
                     <p>{this.props.review.recommend === 0 ? <><FaCheck size='.75em'/>I recommend this product</> : null}</p>
                     {this.props.review.response === null ? null : <p className='review-response'>Response from seller: {this.props.review.response}</p>}
                     <p>Helpful? ({this.props.review.helpfulness})    |    Report Link HERE</p> 
