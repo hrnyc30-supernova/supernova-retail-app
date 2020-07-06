ea4357fe2d41f66bea3cba8b89e295d3b91f2bdb
Shannon Connolly <shannonconnolly92@gmail.com>
2020-07-06 08:06:41 -0400

    Add ability to add up to 5 photos to each review

--- src/components/ratings-and-reviews/uploadPhotos.js
+++ src/components/ratings-and-reviews/uploadPhotos.js
@@ -1,16 +1,12 @@
 import React from "react";
-import Stars from "./stars.js";
 import Modal from "react-bootstrap/Modal";
-import {charScales} from "./constants.js";
-import apiMaster from "../../apiMaster.js";
 
 class UploadPhotos extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      images: [],
     };
-    
   }
 
   render() {
@@ -22,20 +18,31 @@ class UploadPhotos extends React.Component {
         centered
       >
         <Modal.Header>
-            <Modal.Title>
+          <Modal.Title>
             <h4>Upload Your Photos</h4>
             <h6>
-                <small>{`About the ${this.props.currentProductName}`}</small>
+              <small>{`About the ${this.props.currentProductName}`}</small>
             </h6>
-            </Modal.Title>
+          </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-            <input type='file'/>
+          <form>
+            <label className='label-container' htmlFor='photo'>Choose up to 5 photos: <br/>
+                <input type="file" id="photo1" name="photo" accept="image/png, image/jpeg"/>
+                <input type="file" id="photo2" name="photo" accept="image/png, image/jpeg"/>
+                <input type="file" id="photo3" name="photo" accept="image/png, image/jpeg"/>
+                <input type="file" id="photo4" name="photo" accept="image/png, image/jpeg"/>
+                <input type="file" id="photo5" name="photo" accept="image/png, image/jpeg"/>
+            </label>
+          </form>
         </Modal.Body>
         <Modal.Footer>
-            <button className='main-action-button' onClick={e => this.props.onClick(e)}>
+          <button
+            className="main-action-button"
+            onClick={(e) => this.props.onClick(e)}
+          >
             Upload
-            </button>
+          </button>
         </Modal.Footer>
       </Modal>
     );
