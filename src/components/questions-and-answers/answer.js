c1a3e014adecc6ad8db4ce8eb34016cee2c44c06
Chris Campos <camposchris18@gmail.com>
2020-07-07 15:41:37 -0400

    Added Date Format Seller is in bold Added pointer style to report, and helpful links

--- src/components/questions-and-answers/answer.js
+++ src/components/questions-and-answers/answer.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import apiMaster from '../../apiMaster';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import moment from 'moment';
 
 const Answer = (props) => {
   const [answers, setAnswer] = useState([]);
@@ -22,12 +23,19 @@ const Answer = (props) => {
     <>
       <>
         {answers.slice(0, answerLen).map((answer) => {
+          let date = moment(answer.date).format('MMMM D, YYYY');
+          let by;
+          answer.answerer_name === 'Seller'
+            ? (by = <b>{answer.answerer_name}</b>)
+            : (by = answer.answerer_name);
+
           return (
             <Card.Body key={answer.answer_id}>
               <Card.Title>A: {answer.body}</Card.Title>
               <Card.Text>
-                By: {answer.answerer_name} | Helpful? <u> YES</u> (
-                {answer.helpfulness}) |<u> Report</u>
+                By: {by}, {date} | Helpful?{' '}
+                <u style={{ cursor: 'pointer' }}> YES</u> ({answer.helpfulness})
+                |<u style={{ cursor: 'pointer' }}> Report</u>
               </Card.Text>
             </Card.Body>
           );
