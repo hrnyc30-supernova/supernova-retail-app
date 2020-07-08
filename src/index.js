370f0060f871b700f3ccc13ad035d7eda163e9b2
Julia McNeill <juliammcneill@gmail.com>
2020-07-08 11:13:06 -0400

    Integrate SASS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './styles.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./styles.scss";
 
-var mountNode = document.getElementById('app');
+var mountNode = document.getElementById("app");
 ReactDOM.render(<App />, mountNode);
