9898422a529781b6b6158b7a4e408c7a04f5bd7b
Vince <vincent.tran.ece@gmail.com>
2022-04-21 11:50:24 -0500

    saves progress

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const API_KEY = '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';
+export const headers = {
+  headers: {
+    Authorization: API_KEY,
+  },
+};
+
+export const url = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfc';
